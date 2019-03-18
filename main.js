5aecc21b1629d0fb7154e46e4e8349c16753eb93
Ehud <renot51@gmail.com>
2019-03-18 15:12:47 +0100

    first exercise:

---
+++ main.js
@@ -0,0 +1,31 @@
+//1
+
+console.log("this is an exercise in console logging");
+
+//2
+
+var theValue = "This is the value";
+console.log(theValue);
+    
+//3
+
+var firstName = "Ehud";
+var lastName = "Hermon";
+console.log(firstName, lastName);
+
+//4
+
+var firstName1 = "John";
+var lastName1 = "Frum";
+var island = "Tanna in Vanuatu";
+var occupation = "Prophet";
+var religion = "Voodoo Cargo Cult";
+console.log(firstName1, lastName1 + " Came to the island", island + " as a", occupation + " and established the infamous", religion, " !");
+
+//5
+
+var nameDec = "John Smith";
+var age = "43 year old";
+var occupation1 = "teacher";
+var location = "Berlin";
+console.log(nameDec + " is a", age, occupation1 + " who lives in ", location );
